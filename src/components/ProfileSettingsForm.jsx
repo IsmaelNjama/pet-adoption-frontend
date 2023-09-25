d60e8acd1c30a68477f74eba0b561d8c3219e190
IsmaelNjama <petermailsnjama@gmail.com>
2023-09-25 23:47:48 +0300

    styling profile form pet page

--- src/components/ProfileSettingsForm.jsx
+++ src/components/ProfileSettingsForm.jsx
@@ -3,45 +3,52 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
+import("../styles/common.css");
+import("../components/buttons/buttons.css");
 
 function ProfileSettingsForm() {
   return (
-    <Form>
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+    <div>
+      <Form className="w-50 profile-form-wrapper">
+        <h1 className="mb-4">Profile Settings</h1>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label className="form-label">Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Group as={Col}>
+            <Form.Label>First name</Form.Label>
+            <Form.Control type="text" placeholder="Your first name" />
+          </Form.Group>
+
+          <Form.Group as={Col}>
+            <Form.Label>Last name</Form.Label>
+            <Form.Control type="password" placeholder="Your last name" />
+          </Form.Group>
+        </Row>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Phone number</Form.Label>
+          <Form.Control type="text" placeholder="Your phone number" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Add short bio</Form.Label>
+          <Form.Control as="textarea" rows={3} />
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-      </Row>
-
-      <Row className="mb-3">
-        <Form.Group as={Col}>
-          <Form.Label>First name</Form.Label>
-          <Form.Control type="email" placeholder="Your first name" />
-        </Form.Group>
-
-        <Form.Group as={Col}>
-          <Form.Label>Last name</Form.Label>
-          <Form.Control type="password" placeholder="Your last name" />
-        </Form.Group>
-      </Row>
-
-      <Form.Group className="mb-3">
-        <Form.Label>Phone number</Form.Label>
-        <Form.Control placeholder="Your phone number" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Add short bio</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-
-      <Button variant="primary">Save</Button>
-    </Form>
+        <Button className="profile-settings-button" variant="primary">
+          Save
+        </Button>
+      </Form>
+    </div>
   );
 }
 
--- src/pages/PetPage.jsx
+++ src/pages/PetPage.jsx
@@ -2,13 +2,61 @@ import React from "react";
 import AdoptButton from "../components/buttons/AdoptButton";
 import FosterButton from "../components/buttons/FosterButton";
 import SavePetButton from "../components/buttons/SavePetButton";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Stack from "react-bootstrap/Stack";
+import Image from "react-bootstrap/Image";
+import("../styles/common.css");
 
 function PetPage() {
   return (
-    <div>
-      <AdoptButton />
-      <FosterButton />
-      <SavePetButton />
+    <div className="pet-page-container">
+      <Container className="pet-image-container">
+        <Col className="pet-image-col" xs={6} md={4}>
+          <Image className="pet-image" src="" roundedCircle />
+        </Col>
+      </Container>
+      <Container className="pet-details-container">
+        <Row>
+          <Col className="pet-detail-col mb-2" xs={6} md={4}>
+            <p className="pet-type">Type:</p>
+          </Col>
+          <Col className="pet-detail-col mb-2" xs={6} md={4}>
+            <p className="pet-breed">Breed:</p>
+          </Col>
+          <Col className="pet-detail-col mb-2" xs={6} md={4}>
+            <p>Dietary restrictions:</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="pet-detail-col mb-2" xs={6} md={4}>
+            <p> Name:</p>
+          </Col>
+          <Col className="pet-detail-col mb-2" xs={6} md={4}>
+            <p>Color:</p>
+          </Col>
+          <Col className="pet-detail-col mb-2" xs={6} md={4}>
+            <p> Weight:</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="pet-detail-col mb-2" xs={6} md={4}>
+            <p>Adoption Status:</p>
+          </Col>
+          <Col className="pet-detail-col mb-2" xs={6} md={4}>
+            <p>Height:</p>
+          </Col>
+          <Col className="pet-detail-col mb-2" xs={6} md={4}>
+            <p>Hypoallergenic:</p>
+          </Col>
+        </Row>
+      </Container>
+      <Stack direction="horizontal" gap={3}>
+        <AdoptButton />
+        <FosterButton />
+        <SavePetButton />
+      </Stack>
     </div>
   );
 }
